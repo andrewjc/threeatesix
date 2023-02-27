315ea0636673549d9b8f738e8df8141e462737d4
Andrew Cranston <andrew.cranston@digio.com.au>
2023-02-27 20:03:18 +1100

    Implement JBE short rel8

--- devices/intel8086/branch.go
+++ devices/intel8086/branch.go
@@ -1,223 +1,244 @@
 package intel8086
 
 import (
-	"fmt"
-	"github.com/andrewjc/threeatesix/common"
+    "fmt"
+    "github.com/andrewjc/threeatesix/common"
 )
 
 func INSTR_CALLF_M16(core *CpuCore, modrm *ModRm) {
-	if modrm.mod == 3 {
-		reg, reg_str := core.registers.registers16Bit[modrm.rm], core.registers.index16ToString(modrm.rm)
+    if modrm.mod == 3 {
+        reg, reg_str := core.registers.registers16Bit[modrm.rm], core.registers.index16ToString(modrm.rm)
 
-		stackPush16(core, uint16(uint16(core.GetIP()+2)))
+        stackPush16(core, uint16(uint16(core.GetIP()+2)))
 
-		core.registers.IP = *reg
-		core.logInstruction(fmt.Sprintf("[%#04x] CALLF %s (%#04x)", core.GetCurrentlyExecutingInstructionAddress(), reg_str, uint16(*reg)))
-	} else {
-		addr := modrm.getAddressMode16(core)
-		stackPush16(core, uint16(uint16(core.GetIP()+2)))
+        core.registers.IP = *reg
+        core.logInstruction(fmt.Sprintf("[%#04x] CALLF %s (%#04x)", core.GetCurrentlyExecutingInstructionAddress(), reg_str, uint16(*reg)))
+    } else {
+        addr := modrm.getAddressMode16(core)
+        stackPush16(core, uint16(uint16(core.GetIP()+2)))
 
-		core.registers.IP = addr
-		core.logInstruction(fmt.Sprintf("[%#04x] CALLF %#04x", core.GetCurrentlyExecutingInstructionAddress(), uint16(addr)))
-	}
+        core.registers.IP = addr
+        core.logInstruction(fmt.Sprintf("[%#04x] CALLF %#04x", core.GetCurrentlyExecutingInstructionAddress(), uint16(addr)))
+    }
 }
 
 func INSTR_DEC_COUNT_JMP_SHORT_ECX(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	var destAddr = uint16(core.registers.IP + 2)
+    var destAddr = uint16(core.registers.IP + 2)
 
-	destAddr = destAddr + uint16(offset)
+    destAddr = destAddr + uint16(offset)
 
-	term1 := uint16(core.registers.CX)
-	term1 = term1 - 1
-	core.registers.CX = uint16(term1)
+    term1 := uint16(core.registers.CX)
+    term1 = term1 - 1
+    core.registers.CX = uint16(term1)
 
-	additional := true
-	extraStr := ""
+    additional := true
+    extraStr := ""
 
-	if core.currentOpCodeBeingExecuted == 0xE0 {
-		extraStr = "ZF=0"
-		additional = !core.registers.GetFlag(ZeroFlag)
-	}
-	if core.currentOpCodeBeingExecuted == 0xE1 {
-		extraStr = "ZF=1"
-		additional = core.registers.GetFlag(ZeroFlag)
-	}
+    if core.currentOpCodeBeingExecuted == 0xE0 {
+        extraStr = "ZF=0"
+        additional = !core.registers.GetFlag(ZeroFlag)
+    }
+    if core.currentOpCodeBeingExecuted == 0xE1 {
+        extraStr = "ZF=1"
+        additional = core.registers.GetFlag(ZeroFlag)
+    }
 
-	core.logInstruction(fmt.Sprintf("[%#04x] LOOP %#04x (SHORT REL8) %s", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr), extraStr))
-	if term1 != 0 && additional {
-		core.registers.IP = uint16(destAddr)
-		core.logInstruction(fmt.Sprintf("[%#04x]   |-> jumped (CX %#04x)", core.GetCurrentlyExecutingInstructionAddress(), core.registers.CX))
-	} else {
-		core.registers.IP = uint16(uint16(core.GetIP() + 2))
-		core.logInstruction(fmt.Sprintf("[%#04x]   |-> not jumped (CX %#04x)", core.GetCurrentlyExecutingInstructionAddress(), core.registers.CX))
-	}
+    core.logInstruction(fmt.Sprintf("[%#04x] LOOP %#04x (SHORT REL8) %s", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr), extraStr))
+    if term1 != 0 && additional {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x]   |-> jumped (CX %#04x)", core.GetCurrentlyExecutingInstructionAddress(), core.registers.CX))
+    } else {
+        core.registers.IP = uint16(uint16(core.GetIP() + 2))
+        core.logInstruction(fmt.Sprintf("[%#04x]   |-> not jumped (CX %#04x)", core.GetCurrentlyExecutingInstructionAddress(), core.registers.CX))
+    }
 
 }
 
 func INSTR_JMP_FAR_PTR16(core *CpuCore) {
-	destAddr, err := core.memoryAccessController.ReadAddr16(uint32(core.GetCurrentCodePointer()) + 1)
-	segment, err := core.memoryAccessController.ReadAddr16(uint32(core.GetCurrentCodePointer()) + 3)
+    destAddr, err := core.memoryAccessController.ReadAddr16(uint32(core.GetCurrentCodePointer()) + 1)
+    segment, err := core.memoryAccessController.ReadAddr16(uint32(core.GetCurrentCodePointer()) + 3)
 
-	core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x:%#04x (FAR_PTR16)", core.GetCurrentlyExecutingInstructionAddress(), segment, destAddr))
-	if err == nil {
-		core.writeSegmentRegister(&core.registers.CS, uint16(segment))
-	}
+    core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x:%#04x (FAR_PTR16)", core.GetCurrentlyExecutingInstructionAddress(), segment, destAddr))
+    if err == nil {
+        core.writeSegmentRegister(&core.registers.CS, uint16(segment))
+    }
 
-	core.registers.IP = uint16(destAddr)
+    core.registers.IP = uint16(destAddr)
 }
 
 func INSTR_JMP_FAR_M16(core *CpuCore, modrm *ModRm) {
-	if modrm.mod == 3 {
-		reg, reg_str := core.registers.registers16Bit[modrm.rm], core.registers.index16ToString(modrm.rm)
-		core.registers.IP = *reg
-		core.logInstruction(fmt.Sprintf("[%#04x] JMP %s (%#04x) (JMP_FAR_M16)", core.GetCurrentlyExecutingInstructionAddress(), reg_str, uint16(*reg)))
-	} else {
-		addr := modrm.getAddressMode16(core)
-		core.registers.IP = addr
-		core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x (JMP_FAR_M16)", core.GetCurrentlyExecutingInstructionAddress(), uint16(addr)))
-	}
+    if modrm.mod == 3 {
+        reg, reg_str := core.registers.registers16Bit[modrm.rm], core.registers.index16ToString(modrm.rm)
+        core.registers.IP = *reg
+        core.logInstruction(fmt.Sprintf("[%#04x] JMP %s (%#04x) (JMP_FAR_M16)", core.GetCurrentlyExecutingInstructionAddress(), reg_str, uint16(*reg)))
+    } else {
+        addr := modrm.getAddressMode16(core)
+        core.registers.IP = addr
+        core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x (JMP_FAR_M16)", core.GetCurrentlyExecutingInstructionAddress(), uint16(addr)))
+    }
 
 }
 
 func INSTR_JMP_NEAR_REL16(core *CpuCore) {
 
-	offset, err := core.memoryAccessController.ReadAddr16(uint32(core.GetCurrentCodePointer()) + 1)
+    offset, err := core.memoryAccessController.ReadAddr16(uint32(core.GetCurrentCodePointer()) + 1)
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	var destAddr = core.registers.IP + 3
+    var destAddr = core.registers.IP + 3
 
-	var destAddrTest = int16(destAddr)
+    var destAddrTest = int16(destAddr)
 
-	destAddr2 := destAddrTest + int16(offset)
+    destAddr2 := destAddrTest + int16(offset)
 
-	core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x (NEAR_REL16)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	core.registers.IP = uint16(destAddr2)
+    core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x (NEAR_REL16)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    core.registers.IP = uint16(destAddr2)
 }
 
 func INSTR_JS_SHORT_REL8(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	tmp := int16(core.registers.IP + 2)
-	destAddr := uint16(tmp + int16(offset))
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
 
-	if core.registers.GetFlag(SignFlag) {
-		core.registers.IP = uint16(destAddr)
-		core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	} else {
-		core.registers.IP = uint16(core.GetIP() + 2)
-		core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	}
+    if core.registers.GetFlag(SignFlag) {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    } else {
+        core.registers.IP = uint16(core.GetIP() + 2)
+        core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    }
 }
 
 func INSTR_JNS_SHORT_REL8(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	tmp := int16(core.registers.IP + 2)
-	destAddr := uint16(tmp + int16(offset))
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
 
-	if !core.registers.GetFlag(SignFlag) {
-		core.registers.IP = uint16(destAddr)
-		core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	} else {
-		core.registers.IP = uint16(core.GetIP() + 2)
-		core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	}
+    if !core.registers.GetFlag(SignFlag) {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    } else {
+        core.registers.IP = uint16(core.GetIP() + 2)
+        core.logInstruction(fmt.Sprintf("[%#04x] JS %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    }
 }
 
 func INSTR_JZ_SHORT_REL8(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	tmp := int16(core.registers.IP + 2)
-	destAddr := uint16(tmp + int16(offset))
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
 
-	if core.registers.GetFlag(ZeroFlag) {
-		core.registers.IP = uint16(destAddr)
-		core.logInstruction(fmt.Sprintf("[%#04x] JZ %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	} else {
-		core.registers.IP = uint16(core.GetIP() + 2)
-		core.logInstruction(fmt.Sprintf("[%#04x] JZ %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	}
+    if core.registers.GetFlag(ZeroFlag) {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x] JZ %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    } else {
+        core.registers.IP = uint16(core.GetIP() + 2)
+        core.logInstruction(fmt.Sprintf("[%#04x] JZ %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    }
 }
 
 func INSTR_JNZ_SHORT_REL8(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	tmp := int16(core.registers.IP + 2)
-	destAddr := uint16(tmp + int16(offset))
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
 
-	if !core.registers.GetFlag(ZeroFlag) {
-		core.registers.IP = uint16(destAddr)
-		core.logInstruction(fmt.Sprintf("[%#04x] JNZ %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	} else {
-		core.registers.IP = uint16(core.GetIP() + 2)
-		core.logInstruction(fmt.Sprintf("[%#04x] JNZ %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	}
+    if !core.registers.GetFlag(ZeroFlag) {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x] JNZ %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    } else {
+        core.registers.IP = uint16(core.GetIP() + 2)
+        core.logInstruction(fmt.Sprintf("[%#04x] JNZ %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    }
+
+}
+
+func INSTR_JBE_SHORT_REL8(core *CpuCore) {
+
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+
+    if err != nil {
+        return
+    }
+
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
+
+    if core.registers.GetFlag(CarryFlag) || core.registers.GetFlag(ZeroFlag) {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x] JBE %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    } else {
+        core.registers.IP = uint16(core.GetIP() + 2)
+        core.logInstruction(fmt.Sprintf("[%#04x] JBE %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    }
 
 }
 
 func INSTR_JCXZ_SHORT_REL8(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	tmp := int16(core.registers.IP + 2)
-	destAddr := uint16(tmp + int16(offset))
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
 
-	if core.registers.CX == 0 {
-		core.registers.IP = uint16(destAddr)
-		core.logInstruction(fmt.Sprintf("[%#04x] JCXZ %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	} else {
-		core.registers.IP = uint16(core.GetIP() + 2)
-		core.logInstruction(fmt.Sprintf("[%#04x] JCXZ %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	}
+    if core.registers.CX == 0 {
+        core.registers.IP = uint16(destAddr)
+        core.logInstruction(fmt.Sprintf("[%#04x] JCXZ %#04x (SHORT REL8) (Jumped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    } else {
+        core.registers.IP = uint16(core.GetIP() + 2)
+        core.logInstruction(fmt.Sprintf("[%#04x] JCXZ %#04x (SHORT REL8) (Skipped)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    }
 
 }
 
 func INSTR_JMP_SHORT_REL8(core *CpuCore) {
 
-	offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
+    offset, err := common.Int8Err(core.memoryAccessController.ReadAddr8(uint32(core.GetCurrentCodePointer()) + 1))
 
-	if err != nil {
-		return
-	}
+    if err != nil {
+        return
+    }
 
-	tmp := int16(core.registers.IP + 2)
-	destAddr := uint16(tmp + int16(offset))
+    tmp := int16(core.registers.IP + 2)
+    destAddr := uint16(tmp + int16(offset))
 
-	core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x (SHORT REL8)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
-	core.registers.IP = uint16(destAddr)
+    core.logInstruction(fmt.Sprintf("[%#04x] JMP %#04x (SHORT REL8)", core.GetCurrentlyExecutingInstructionAddress(), uint16(destAddr)))
+    core.registers.IP = uint16(destAddr)
 
 }
