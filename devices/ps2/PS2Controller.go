6b4ca0d2733d70fda310e2547f0a638bd4f42a2c
Andrew Cranston <andrew.cranston@digio.com.au>
2024-04-03 23:34:50 +1100

    ps2 controller

--- devices/ps2/PS2Controller.go
+++ devices/ps2/PS2Controller.go
@@ -6,12 +6,11 @@ import (
 )
 
 type Ps2Controller struct {
-	bus                *bus.Bus
-	busId              uint32
-	statusRegister     uint8
-	bufferedOutputData []uint8
-	bufferedInputData  []uint8
-
+	bus                  *bus.Bus
+	busId                uint32
+	statusRegister       uint8
+	bufferedOutputData   []uint8
+	bufferedInputData    []uint8
 	internalRam          uint8 // used for storing config bytes
 	pendingOperation     uint8
 	port1_enabled        bool
@@ -31,7 +30,6 @@ func (controller *Ps2Controller) SetDeviceBusId(id uint32) {
 }
 
 func (controller *Ps2Controller) OnReceiveMessage(message bus.BusMessage) {
-
 }
 
 func CreatePS2Controller() *Ps2Controller {
@@ -44,6 +42,20 @@ func (controller *Ps2Controller) GetBus() *bus.Bus {
 
 func (controller *Ps2Controller) SetBus(bus *bus.Bus) {
 	controller.bus = bus
+}
+
+func (controller *Ps2Controller) WriteDataPort(value uint8) {
+	if controller.isCommand {
+		controller.WriteCommandRegister(value)
+	} else {
+		// The value is intended for the device plugged into PS2 PORT 0 or 1
+		log.Printf("PS2 Port Output Data: [%#04x]", value)
+		// Send the value to the appropriate PS2 port based on the current configuration
+	}
+}
+
+func (controller *Ps2Controller) WriteControlPort(value uint8) {
+	controller.WriteCommandRegister(value)
 }
 
 func (controller *Ps2Controller) ReadStatusRegister() uint8 {
@@ -108,37 +120,31 @@ func (controller *Ps2Controller) updateStatusRegister() {
 	}
 
 	// Bit 2: System Flag
-	// Assuming that system flag is represented by a bool variable 'systemFlag'
 	if controller.systemFlag {
 		controller.statusRegister |= 0x04 // Set System Flag
 	}
 
 	// Bit 3: Command/Data
-	// Assuming a bool variable 'isCommand' indicates whether last write was a command
 	if controller.isCommand {
 		controller.statusRegister |= 0x08 // Set Command/Data
 	}
 
 	// Bit 4: Keyboard Lock
-	// Assuming a bool variable 'keyboardLocked' indicates the state of keyboard lock
 	if controller.keyboardLocked {
 		controller.statusRegister |= 0x10 // Set Keyboard Lock
 	}
 
 	// Bit 5: Auxiliary Output Buffer Full
-	// Assuming a bool variable 'auxiliaryBufferFull' indicates the state of the auxiliary buffer
 	if controller.auxiliaryBufferFull {
 		controller.statusRegister |= 0x20 // Set Auxiliary Output Buffer Full
 	}
 
 	// Bit 6: Time-out
-	// Assuming a bool variable 'timeout' indicates the state of time-out
 	if controller.timeout {
 		controller.statusRegister |= 0x40 // Set Time-out
 	}
 
 	// Bit 7: Parity error
-	// Assuming a bool variable 'parityError' indicates the state of parity error
 	if controller.parityError {
 		controller.statusRegister |= 0x80 // Set Parity error
 	}
@@ -147,7 +153,7 @@ func (controller *Ps2Controller) updateStatusRegister() {
 func (controller *Ps2Controller) ReadDataPort() uint8 {
 	if len(controller.bufferedOutputData) > 0 {
 		response := controller.bufferedOutputData[0]
-		controller.bufferedOutputData = nil
+		controller.bufferedOutputData = controller.bufferedOutputData[1:]
 		controller.DisableDataPortReadyForRead()
 		return response
 	}
@@ -157,62 +163,50 @@ func (controller *Ps2Controller) ReadDataPort() uint8 {
 func (controller *Ps2Controller) WriteCommandRegister(value uint8) {
 	if controller.isCommand {
 		log.Printf("PS2 controller write command: [%#04x]", value)
-		if value == 0xAA {
-			// Test PS2 Controller
+		if value == 0xAA { // Test PS2 Controller
 			controller.SendBufferedResponse([]uint8{0x55}) //OK Message
 			return
 		}
-
-		if value == 0xAD {
-			// Disable first ps2 port
+		if value == 0xAD { // Disable first ps2 port
 			controller.port1_enabled = false
 			controller.DisableDataPortReadyForRead()
 			controller.DisableDataPortReadyForWrite()
 			return
 		}
-
-		if value == 0xAE {
-			// Enable first ps2 port
+		if value == 0xAE { // Enable first ps2 port
 			controller.port1_enabled = true
 			controller.EnableDataPortReadyForRead()
 			controller.EnableDataPortReadyForWrite()
 			return
 		}
-
-		if value == 0xA7 {
-			// Disable second ps2 port
+		if value == 0xA7 { // Disable second ps2 port
 			controller.port2_enabled = false
 			return
 		}
-
-		if value == 0xA8 {
-			// Enable second ps2 port
+		if value == 0xA8 { // Enable second ps2 port
 			controller.port2_enabled = true
 			return
 		}
-
-		if value == 0x60 {
-			// Write next byte to byte 0 of internal data
+		if value == 0x60 { // Write next byte to byte 0 of internal data
 			controller.pendingOperation = 0x60
 			controller.EnableDataPortReadyForWrite()
 			return
 		}
-
 		if controller.pendingOperation == 0x60 {
-			controller.resetStatusRegister(value)
+			controller.internalRam = value
+			controller.pendingOperation = 0
 			return
 		}
-
 		log.Printf("Unknown PS2 controller write command: [%#04x]", value)
 	} else {
 		// The value is intended for the device plugged into PS2 PORT 0 or 1
 		log.Printf("PS2 Port Output Data: [%#04x]", value)
+		// Send the value to the appropriate PS2 port based on the current configuration
 	}
-
 }
 
 func (controller *Ps2Controller) SendBufferedResponse(response []uint8) {
-	controller.bufferedOutputData = response
+	controller.bufferedOutputData = append(controller.bufferedOutputData, response...)
 	controller.EnableDataPortReadyForRead()
 }
 
@@ -225,6 +219,7 @@ func (controller *Ps2Controller) DisableDataPortReadyForWrite() {
 	controller.dataPortWriteEnabled = false
 	controller.updateStatusRegister()
 }
+
 func (controller *Ps2Controller) EnableDataPortReadyForRead() {
 	controller.dataPortReadEnabled = true
 	controller.updateStatusRegister()
@@ -233,14 +228,4 @@ func (controller *Ps2Controller) EnableDataPortReadyForRead() {
 func (controller *Ps2Controller) DisableDataPortReadyForRead() {
 	controller.dataPortReadEnabled = false
 	controller.updateStatusRegister()
-}
-
-func (controller *Ps2Controller) WriteDataPort(value uint8) {
-	if controller.dataPortWriteEnabled {
-		log.Printf("PS2 controller write data: [%#04x]", value)
-		controller.bufferedInputData = append(controller.bufferedInputData, value)
-		controller.DisableDataPortReadyForWrite()
-	} else {
-		log.Printf("PS2 controller write data: [%#04x] (ignored)", value)
-	}
 }
